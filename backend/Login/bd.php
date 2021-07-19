ffe3520c2bfd8646fb64d7861ed97e259c3fb410
jhongonzales <jhonago455@gmail.com>
2021-07-18 19:54:45 -0500

    [#2]validar_login

--- backend/Login/bd.php
+++ backend/Login/bd.php
@@ -1,9 +1,10 @@
 <?php
+$server = "bnylrz2kdcpkhneftgj8-mysql.services.clever-cloud.com:20721";
 $contrasenia = "vSw6Lg10TGN52oui0vW";
 $usuario = "uwy8uzseqzphuwgl";
 $nombre_bd = "bnylrz2kdcpkhneftgj8";
 try{
-    return new PDO('mysql:host=bnylrz2kdcpkhneftgj8-mysql.services.clever-cloud.com:20721;dbname='. $nombre_bd, $usuario, $contrasenia);
+    $conn = new PDO('mysql:host=$server;dbname=$nombre_bd;',$usuario,$contrasenia);
 }catch(Exception $e){
     echo "Ocurrió algo con la base de datos: ".$e->getMessage();
 }
